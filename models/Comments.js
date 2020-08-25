da036cc9431e96a5caed9bb6bf940d68a266c7ef
Spartacus <k_morter@outlook.com>
2020-08-25 08:36:02 -0600

    added replies schema and routes

--- models/Comments.js
+++ models/Comments.js
@@ -1,4 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReplySchema = new Schema({
+    //set custome id to avoid condustion with parent comment_id
+    replyId:{
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    replyBody: {
+    type: String
+    },
+    writtenBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+},
+    {
+        toJSON: {
+            getters: true
+        }
+    } 
+)
 
 const CommentSchema = new Schema({
     writtenBy: {
@@ -9,9 +35,25 @@ const CommentSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
-    }
-});
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    replies: [ReplySchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    } 
+);
+
+// get total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+  });
+
 
 const Comment = model('Comment', CommentSchema);
 
